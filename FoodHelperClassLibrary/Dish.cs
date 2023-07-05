901ab454b3086cf9ed5f9e4993396f2a2137d4e0
emmiii <70427470+emmiii@users.noreply.github.com>
2023-07-05 22:18:36 +0200

    Add files via upload

---
+++ FoodHelperClassLibrary/Dish.cs
@@ -0,0 +1,19 @@
+﻿namespace FoodHelperClassLibrary
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public ProteinOptions Protein { get; set; }
+        public CarbOptions Carb { get; set; }
+        public EffortOptions Effort { get; set; }
+        public string Recipe { get; set; }
+        public int Minutes { get; set; }
+
+        public Dish()
+        {
+            Protein = ProteinOptions.Chicken;
+            Carb = CarbOptions.Pasta;
+            Effort = EffortOptions.Low;
+        }
+    }
+}
